b3fdc1a5bcde408a6c7a0e93eb26fc52b84270fb
ajani <noreply>
2015-03-26 21:14:52 +0000

    Add component Title

---
+++ ui/title.reel/title.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/title.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Title
+ * @extends Component
+ */
+exports.Title = Component.specialize(/** @lends Title# */ {
+    constructor: {
+        value: function Title() {
+            this.super();
+        }
+    }
+});
